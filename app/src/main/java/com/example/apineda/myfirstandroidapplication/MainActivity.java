6442110dbae3cf4e6c1c7569891f4638b426a929
Adriana Pineda <am.pineda206@uniandes.edu.co>
2015-10-03 11:01:03 -0500

    Add share button and ListView

--- app/src/main/java/com/example/apineda/myfirstandroidapplication/MainActivity.java
+++ app/src/main/java/com/example/apineda/myfirstandroidapplication/MainActivity.java
@@ -1,19 +1,35 @@
 package com.example.apineda.myfirstandroidapplication;
 
+import android.content.Intent;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.ShareActionProvider;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+import java.util.ArrayList;
 
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, AdapterView.OnItemClickListener {
+
+    // UI elements
     TextView mainTextView;
     Button button;
     EditText editText;
+    ListView listView;
+    ShareActionProvider shareAction;
+
+    // Logic elements
+    ArrayAdapter arrayAdapter;
+    ArrayList nameList = new ArrayList();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,18 +43,60 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         button.setOnClickListener(this);
 
         editText = (EditText) findViewById(R.id.edit_text);
+
+        listView = (ListView) findViewById(R.id.list_view);
+
+        arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, nameList);
+        listView.setAdapter(arrayAdapter);
+        listView.setOnItemClickListener(this);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        MenuItem shareItem = menu.findItem(R.id.menu_item_share);
+        if (shareItem != null) {
+            shareAction = (ShareActionProvider) MenuItemCompat.getActionProvider(shareItem);
+        }
+
+        setShareIntent();
+
         return true;
+    }
+
+    private void setShareIntent() {
+
+        if (shareAction != null) {
+
+            // create an Intent with the contents of the TextView
+            Intent shareIntent = new Intent(Intent.ACTION_SEND);
+            shareIntent.setType("text/plain");
+            shareIntent.putExtra(Intent.EXTRA_SUBJECT, "Android Development");
+            shareIntent.putExtra(Intent.EXTRA_TEXT, mainTextView.getText());
+
+            // Make sure the provider knows
+            // it should work with that Intent
+            shareAction.setShareIntent(shareIntent);
+        }
     }
 
     @Override
     public void onClick(View v) {
         String text = editText.getText().toString();
-        mainTextView.setText(text);
+        mainTextView.setText(text + " is learning Android!");
+        editText.getText().clear();
+
+        nameList.add(text);
+        arrayAdapter.notifyDataSetChanged();
+
+        setShareIntent();
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+        Log.d("omg android", position + ":" + nameList.get(position));
     }
 }
